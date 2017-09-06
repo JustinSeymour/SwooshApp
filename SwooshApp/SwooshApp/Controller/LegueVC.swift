251f605ad094dde13646706a6c7c69d613ae0a8f
Justin Seymour <justin@3wmholdings.com>
2017-09-06 10:08:33 +0200

    Final commit

--- SwooshApp/SwooshApp/Controller/LegueVC.swift
+++ SwooshApp/SwooshApp/Controller/LegueVC.swift
@@ -40,4 +40,10 @@ class LegueVC: UIViewController {
         player.desiredLeague = legueType
         nextBtn.isEnabled = true
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
 }
